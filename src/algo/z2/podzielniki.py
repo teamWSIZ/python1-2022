99f923c5a64cee0a76458b95da8f7761ad7313a2
PolishTanker <wojciech.borak@gmail.com>
2023-03-18 14:39:12 +0100

    poprawka codu

--- src/algo/z2/podzielniki.py
+++ src/algo/z2/podzielniki.py
@@ -3,13 +3,12 @@ from random import seed, randint
 from automat import run_tests, visualize
 
 
-def solve(data):
+def solve(a: list[int], b: list[int]) -> list[int]:
     result = []
 #Wersja list
     # a, b = data[::2], data[1::2]
 #Wersja set
-    a = list(set(data[::2]))
-    b = list(set(data[1::2]))
+    
     for i in range (len(a)):
         counter = 0
         for j in range (len(b)):
@@ -22,7 +21,9 @@ def generate_data(data_size):
     data_list = []
     for _ in range(data_size*2):
         data_list.append(randint(1,100))
-    return data_list
+        a = list(set(data_list[::2]))
+        b = list(set(data_list[1::2]))
+    return {"a": a, "b": b}
 
 if __name__ == '__main__':
     x, y = run_tests(generate_data, solve, max_size=10**4)
