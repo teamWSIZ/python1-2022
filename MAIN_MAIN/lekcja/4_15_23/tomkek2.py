50f573802b7dcd82387029f7aa0341e28b121ed8
tomo2515x <tomo2515pl@gmail.com>
2023-04-22 14:34:32 +0200

    Update tomkek2.py

--- MAIN_MAIN/lekcja/4_15_23/tomkek2.py
+++ MAIN_MAIN/lekcja/4_15_23/tomkek2.py
@@ -1,6 +1,10 @@
+# A
+
 def isUnique(li):
     if len(li) <= len(set(li)): return True
     return False
+
+# B
 
 def lookForCulprit(li):
     blames = set()
@@ -14,3 +18,19 @@ def lookForCulprit(li):
 
 print(isUnique([2,2,3,5,6,7]))
 print(lookForCulprit([2,2,3,3,5,6,7]))
+
+# C i D
+
+def id_exists(lista):
+    for x in range(len(lista)):
+        id = lista[x][0]
+        for y in range(x+1, len(lista)):
+            if id == lista[y][0]:
+                return True
+    return False
+
+
+lista = [(5,'Adam'), (3,'Jane'), (5, 'Xiao'), (2,'Jane')]
+
+print(id_exists(lista))
+
