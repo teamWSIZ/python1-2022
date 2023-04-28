2f1b8f6e0ec9259eda466c6c8722b4e29b39670d
tomo2515x <tomo2515pl@gmail.com>
2023-04-28 16:30:31 +0200

    Update tomkek2.py

--- MAIN_MAIN/lekcja/4_15_23/tomkek2.py
+++ MAIN_MAIN/lekcja/4_15_23/tomkek2.py
@@ -10,7 +10,29 @@ def lookForCulprit(li):
         return False
 
     print("blame",blames)
-    return True
+    return True, blames
 
-print(isUnique([2,2,3,5,6,7]))
-print(lookForCulprit([2,2,3,3,5,6,7]))
+# print(isUnique([2,2,3,5,6,7]))
+# print(lookForCulprit([2,2,3,3,5,6,7]))
+
+people = [[1, "Jane"], [2, "Jane"], [2, "Amon"], [3, "James"], [2, "Anit"], [2, "Amir"]]
+
+def NotUniqueIds(li):
+    Culprit = []
+    PrevEl = [0]
+    for elem in li:
+        print(elem[0])
+        if elem[0] == PrevEl[0]:
+            Culprit.append([elem, PrevEl])
+            
+        PrevEl = elem
+        print(PrevEl)
+        print(Culprit)
+    # for cul in lookForCulprit(SeenIds[1])[1]:
+    #     print(cul)
+
+    
+        
+        
+
+print(NotUniqueIds(people))
