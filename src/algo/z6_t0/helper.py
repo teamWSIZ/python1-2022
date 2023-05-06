33c1304660e53c37a68fcf3a6825f934699aea1f
Algorytmix1337 <kontodogithuba@o2.pl>
2023-05-06 15:20:39 +0200

    Dodawnie ulamkow

--- src/algo/z6_t0/helper.py
+++ src/algo/z6_t0/helper.py
@@ -2,9 +2,9 @@ from model import Fraction
 
 
 def add(f1: Fraction, f2: Fraction) -> Fraction:
-    # write me
-    return f1  # obviously false
+     # ułamek = numer / denom
+   return 
 
-
+#asdasdfaqwdfaqweffawfawqf
 
 #test
