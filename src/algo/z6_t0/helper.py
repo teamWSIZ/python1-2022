e5df01b1461f03d9fcac6b9d7aeec70e24b110e5
snacksncode <snacksncode@gmail.com>
2023-05-06 21:44:54 +0200

    refactor: remove unnecessary getters, make tests runnable

--- src/algo/z6_t0/helper.py
+++ src/algo/z6_t0/helper.py
@@ -2,11 +2,11 @@ from model import Fraction
 
 
 def add(f1: Fraction, f2: Fraction) -> Fraction:
-    if f1.getDenominator() == f2.getDenominator():
-        return Fraction(f1.getNumerator() + f2.getNumerator(), f1.getDenominator())
+    if f1.denom == f2.denom:
+        return Fraction(f1.numer + f2.numer, f1.denom)
 
-    newDenominator = f1.getDenominator() * f2.getDenominator()
-    newF1Numerator = f1.getNumerator() * f2.getDenominator()
-    newF2Numerator = f2.getNumerator() * f1.getDenominator()
+    newDenominator = f1.denom * f2.denom
+    newF1Numerator = f1.numer * f2.denom
+    newF2Numerator = f2.numer * f1.denom
 
     return Fraction(newF1Numerator + newF2Numerator, newDenominator)
--- src/algo/z6_t0/model.py
+++ src/algo/z6_t0/model.py
@@ -10,12 +10,6 @@ class Fraction:
     def value(self):
         return self.numer / self.denom
 
-    def getDenominator(self) -> int:
-        return self.denom
-
-    def getNumerator(self) -> int:
-        return self.numer
-
 
 if __name__ == '__main__':
     f = Fraction(1, 2)
