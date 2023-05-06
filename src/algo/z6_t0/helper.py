3a62bc4e54d1019613404bc19372a10e672bb297
snacksncode <snacksncode@gmail.com>
2023-05-06 14:37:10 +0200

    feat: implement add function

--- src/algo/z6_t0/helper.py
+++ src/algo/z6_t0/helper.py
@@ -2,5 +2,11 @@ from model import Fraction
 
 
 def add(f1: Fraction, f2: Fraction) -> Fraction:
-    # write me
-    return f1  # obviously false
+    if f1.getDenominator() == f2.getDenominator():
+        return Fraction(f1.getNumerator() + f2.getNumerator(), f1.getDenominator())
+
+    newDenominator = f1.getDenominator() * f2.getDenominator()
+    newF1Numerator = f1.getNumerator() * f2.getDenominator()
+    newF2Numerator = f2.getNumerator() * f1.getDenominator()
+
+    return Fraction(newF1Numerator + newF2Numerator, newDenominator)
