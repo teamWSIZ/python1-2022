777dd243b3074bc4c2ae0894e49e6bd1231fcf47
DrGunterII <93789329+DrGunterII@users.noreply.github.com>
2023-05-06 15:07:11 +0200

    Update zad_T0.py

--- MAIN_MAIN/lekcja/T0/zad_T0.py
+++ MAIN_MAIN/lekcja/T0/zad_T0.py
@@ -11,18 +11,20 @@ class ulamek_2:
     mianownik_2 = int
 
 
-def dodawanie_ulamków(licznik_1, mianownik_1, licznik_2, mianownik_2):
-    if mianownik_1 == mianownik_2:
-        suma_licznika = licznik_1 + licznik_2
-        print("suma tych ułamów wynosi:", suma_licznika, "/", mianownik_1)
+def dodawanie_ulamkow(liczniki_1, mianowniki_1, liczniki_2, mianowniki_2):
+    if mianowniki_1 == mianowniki_2:
+        suma_licznika = liczniki_1 + liczniki_2
+        print("suma tych ułamów wynosi:", suma_licznika, "/", mianowniki_1)
     else:
-        iloraz_licznika_1 = licznik_1 * mianownik_2
-        iloraz_mianownika_1 = mianownik_1 * mianownik_2
-        iloraz_licznika_2 = licznik_2 * mianownik_1
-        iloraz_mianownika_2 = mianownik_2 * mianownik_1
+        iloraz_licznika_1 = liczniki_1 * mianowniki_2
+        iloraz_mianownika_1 = mianowniki_1 * mianowniki_2
+        iloraz_licznika_2 = liczniki_2 * mianowniki_1
+        iloraz_mianownika_2 = mianowniki_2 * mianowniki_1
 
         suma_licznikow = iloraz_licznika_2 + iloraz_licznika_1
         print("suma tych ułamów wynosi:", suma_licznikow, "/", iloraz_mianownika_2)
 
 
-print(dodawanie_ulamków(1, 2, 2, 5))
+licznik_1, mianownik_1 = input(int)("Wpisz pierszy ułamek do dodawania")
+licznik_2, mianownik_2 = input("Wpisz drógi ułamek do dodawania")
+print(dodawanie_ulamkow(licznik_1, mianownik_1, licznik_2, mianownik_2))
