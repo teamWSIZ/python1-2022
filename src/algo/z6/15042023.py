004fa7cdd5e26a951784cea669d8ded82460e7ea
dawidbcknd <127606289+dawidbcknd@users.noreply.github.com>
2023-05-13 12:30:38 +0200

    Update 15042023.py

--- src/algo/z6/15042023.py
+++ src/algo/z6/15042023.py
@@ -80,3 +80,18 @@ unique(list3)
 list4 = ['aa', 'a', 'c', 'bb', 'bb', 'a']
 print(list4)
 unique(list4)
+
+
+data = [(5,'Adam'), (3,'Jane'), (5, 'Xiao'), (2,'Jane')]
+
+d = {}
+
+for id, name in data:
+    if name not in d:
+        d[name] = [id]
+    else:
+        d[name].append(id)
+
+for name, ids in d.items():
+    if len(ids) > 1:
+        print(f"Dla imienia {name} występuje więcej niż jednego id: {ids}")
