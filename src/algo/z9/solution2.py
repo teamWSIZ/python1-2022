c5c1cda80e876d0afaff404ef63894cfd736f9af
dawidlaciak <dawidlaciak02@gmail.com>
2023-05-28 00:10:08 +0200

    solution2.py

--- src/algo/z9/solution2.py
+++ src/algo/z9/solution2.py
@@ -16,8 +16,25 @@ def get_latest(versions: list[str]) -> str:
     """
     :return: Latest semantic version from the given `versions`
     """
-    # todo: your code
-    return '0.0.1'
+    latest_version = versions[0]
+    
+    for version in versions[1:]:
+        if is_higher_version(version, latest_version):
+            latest_version = version
+
+    return latest_version
+
+def is_higher_version(version: str, compared_version: str) -> bool:
+    version_parts = version.split('.')
+    compared_parts = compared_version.split('.')
+
+    for i in range(3):
+        if int(version_parts[i]) > int(compared_parts[i]):
+            return True
+        elif int(version_parts[i]) < int(compared_parts[i]):
+            return False
+
+    return False
 
 
 def next_version(version: str, level: int) -> str:
@@ -31,10 +48,13 @@ def next_version(version: str, level: int) -> str:
         version_list[i] = int(version_list[i])
     if level == 0:
         version_list[0] += 1
+        version_list[1] = 0
+        version_list[2] = 0
     elif level == 1:
         version_list[1] += 1
+        version_list[2] = 0
     elif level == 2:
         version_list[2] += 1
 
-    final_version = version_list[0] + '.' + version_list[1]+ '.' + version_list[2]
+    final_version = str(version_list[0]) + '.' + str(version_list[1])+ '.' + str(version_list[2])
     return final_version
