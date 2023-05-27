8777c6f70d85bc8f253d40ce02308bd79aa435e9
dawidlaciak <dawidlaciak02@gmail.com>
2023-05-27 23:23:37 +0200

    solution2.py

--- src/algo/z9/solution2.py
+++ src/algo/z9/solution2.py
@@ -26,5 +26,15 @@ def next_version(version: str, level: int) -> str:
     :param level: Which part should be incremented; 0: major, 1: minor, 2: patch
     :return: Properly incremented version
     """
-    # todo: your code
-    return '0.0.1'
+    version_list = version.split('.')
+    for i in range(len(version_list)):
+        version_list[i] = int(version_list[i])
+    if level == 0:
+        version_list[0] += 1
+    elif level == 1:
+        version_list[1] += 1
+    elif level == 2:
+        version_list[2] += 1
+
+    final_version = version_list[0] + '.' + version_list[1]+ '.' + version_list[2]
+    return final_version
