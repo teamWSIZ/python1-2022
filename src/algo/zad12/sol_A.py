f907196e0d4cad17e46c9015bb599f58c7a03e1e
PolishTanker <wojciech.borak@gmail.com>
2023-06-24 14:40:18 +0200

    zad12 resolve

--- src/algo/zad12/sol_A.py
+++ src/algo/zad12/sol_A.py
@@ -1,3 +1,14 @@
 def get_min_number_of_operations(a: list[int]) -> int:
-    # todo: your code here
-    return 0
+    count = 0
+    length = len(a)
+    end = 0
+    while length > end:
+        if a[end] < 0:
+            count = count +1
+            while a[end] <= 0:
+                   end = end+1
+                   if length == end: break
+        end = end +1
+    return count
+
+print(get_min_number_of_operations([1, 0, -1, -1, 0, 2, -2, -2, -2]))
